1ecd10bc257225815bcda080b407a547da38916a
Ahmad Arab <ahmadarab45521@gmail.com>
2021-12-26 22:46:17 +0200

    Chatlist

---
+++ Asayesh Messanger/Asayesh Messanger/Controls/Chat/ChatListControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Controls;
+
+
+namespace Asayesh_Messanger
+{
+    /// <summary>
+    /// Interaction logic for ChatListControl.xaml
+    /// </summary>
+    public partial class ChatListControl : UserControl
+    {
+        public ChatListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Asayesh Messanger/Asayesh Messanger/ValueConverters/StringRGBToBrushConverter.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace Asayesh_Messanger
+{
+    public class StringRGBToBrushConverter : BaseValueConverter<StringRGBToBrushConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (SolidColorBrush)(new BrushConverter().ConvertFrom($"#{value}"));
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
